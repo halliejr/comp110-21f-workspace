caf9dda208c1bfa31e668ff6255a6c128211da66
halliejr <halliejr@live.unc.edu>
2021-08-23 19:06:46 -0400

    Finished Exercise 00!

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,5 +1,6 @@
-print("Hello, world.")
-print("Hello, world of humans who love chocolate")
-"""My first program for COMP110"""
+"""My first assignment for COMP110."""
 
 730249177
+
+print("Hello, world.")
+print("Hello, world of humans who love chocolate")
