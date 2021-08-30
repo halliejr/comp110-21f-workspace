b62eb3cdfeb4053434a099fcc2f276e9da590ea8
halliejr <halliejr@live.unc.edu>
2021-08-30 01:45:17 -0400

    Exercise01

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,8 +1,8 @@
 """A program to hype me up!"""
 
-__author__ = 730249177
+__author__ = "730249177"
 
 name: str = input("Who are you? ")
-print(name + " is the smartest girl in this class ")
+print("Wow ", name + " is the smartest girl in this class ")
 print(name + ", you are going to rock the PCAT!")
-print(name + " is going to ace her exams this semester!")
+print("Who is going to ace her exams this semester? " + name + "!")
--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -1,14 +1,14 @@
 """Relational Expressions for the cool kids!"""
 
-__author__ = 730249177
+__author__ = "730249177"
 
 left: int = int(input("Left-hand side: "))
 right: int = int(input("Right-hand side: "))
 answer1: bool = left < right
-answer2: bool = left > right
+answer2: bool = left >= right
 answer3: bool = left == right
 answer4: bool = left != right
 print(str(left) + " < " + str(right) + " is " + str(answer1))
-print(str(left) + " > " + str(right) + " is " + str(answer2))
+print(str(left) + " >= " + str(right) + " is " + str(answer2))
 print(str(left) + " == " + str(right) + " is " + str(answer3))
 print(str(left) + " != " + str(right) + " is " + str(answer4))
