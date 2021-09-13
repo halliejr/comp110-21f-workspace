6c7e440bc390cff9fd75692d21ece3995029fe07
halliejr <halliejr@live.unc.edu>
2021-09-13 17:09:14 -0400

    final

--- exercises/ex02/fortune_cookie.py
+++ exercises/ex02/fortune_cookie.py
@@ -2,16 +2,17 @@
 
 __author__ = "730249177"
 from random import randint
+
 print("Your fortune cookie says...")
 choice = randint(1, 100)
-if choice < 25:
+if choice < 50:
     print("An old love will come back to you in the coming days.")
 else: 
-    if choice < 50:
+    if choice <= 30:
         print("The specific thing that you have wished for will come true.")
     else:
-        if choice < 75:
+        if choice <= 10:
             print("Life is about to be better than it has ever been for you.")
         else: 
-            print("You will be rewarded for all of your hard work.")
+            print("A fresh start will put you on your way.")
 print("Now, go spread positive vibes!")
