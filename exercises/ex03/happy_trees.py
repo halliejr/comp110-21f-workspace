3194ed83b8c530bf36d7a0acaa5b7d9b80fc2da2
halliejr <halliejr@live.unc.edu>
2021-09-14 18:02:53 -0400

    Exercise 3

--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,18 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730249177"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+
+space1: str = " " + TREE
+depth: int = int(input("Depth: "))
+
+if depth > 0:
+    print(TREE)
+    while depth > 1:
+        depth = depth - 1
+        TREE = TREE + space1
+        print(TREE)
+else:
+    print(None)
--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,21 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730249177"
 
 
-# Begin your solution here...
+number: int = int(input("Enter an int:"))
+
+if ((number % 2) == 0) and ((number % 7) == 0):
+    # number is divisible by 2 and 7
+    print("TAR HEELS")
+else:
+    if (number % 2) == 0:
+        # number is divisible by 7
+        print("TAR")
+    else:
+        if (number % 7) == 0:
+            # number is divisible by 7
+            print("HEELS")                      
+        else:
+            # number is not divisible by 2 or 7
+            print("CAROLINA")
