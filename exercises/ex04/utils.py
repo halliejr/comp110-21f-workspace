aa27f85968da965e5769c7001e63c86dd691e11b
halliejr <halliejr@live.unc.edu>
2021-09-27 15:26:21 -0400

    EX04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -4,3 +4,53 @@ __author__ = "123456789"
 
 
 # TODO: Implement your functions here.
+
+
+def main() -> None:
+    """Import Numbers for List."""
+    print(all([1, 1, 1], 1))
+    print(is_equal([1, 0, 1], [1, 1, 1]))
+    print(max([100, 99, 101]))
+    return None 
+
+
+def all(x: list[int], y: int) -> bool:
+    """All will return a bool indicating True or False."""
+    start_len: int = len(x)
+    i: int = 0
+    h: bool = True
+    while i < start_len:
+        if x[i] == y:
+            return h
+        i += 1       
+    else: 
+        return False
+
+
+def is_equal(x: list[int], y: list[int]):
+    """See if list values are equal."""
+    list_leny: int = len(y)
+    i: int = 0
+    h: bool = True
+    while i < list_leny:
+        if x[i] == y:
+            return h
+        i += 1
+    else:
+        return False
+
+
+def max(input: list[int]) -> int:
+    """Return the maximum number in the list."""
+    i: int = 0
+    j: int = 101
+    if len(input) != 0:
+        while input[i] <= 1:
+            return j
+    else:
+        raise ValueError("max() arg is an empt list")
+    return j
+
+
+if __name__ == "__main__":
+    main()
